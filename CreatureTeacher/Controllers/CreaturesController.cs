a7d68696a103f83a9f31b7761e8d8131ecb223b1
Katy Solovewicz <katy@solovewicz.com>
2020-04-06 16:42:03 -0700

    add createchild routes and CodonScrambler method
    > Co-authored-by: Matt Taylor <taylor.matt@protonmail.com>
    > Co-authored-by: spodolak <stephpodolak@gmail.com>
    > Co-authored-by: Jeremy Kale Padot <jeremy@padot.us>

--- CreatureTeacher/Controllers/CreaturesController.cs
+++ CreatureTeacher/Controllers/CreaturesController.cs
@@ -18,7 +18,7 @@ namespace CreatureTeacher.Controllers
 
     public ActionResult Index()
     {
-      List<Creature> model = _db.Creatures.ToList();
+      List<Creature> model = _db.Creatures.Include(creat => creat.Head).ToList();
       return View(model);
     }
     public ActionResult Create()
@@ -32,6 +32,54 @@ namespace CreatureTeacher.Controllers
       _db.Creatures.Add(creature);
       _db.SaveChanges();
       return RedirectToAction("Index");
+    }
+
+    public ActionResult CreateChild()
+    {
+      return View();
+    }
+    public ActionResult Edit(int id)
+    {
+      var thisStudent = _db.Students.FirstOrDefault(students => students.StudentId == id);
+      ViewBag.CourseId = new SelectList(_db.Courses, "CourseId", "CourseName");
+      return View(thisStudent);
+      
+    [HttpPost]
+    public ActionResult CreateChild(int parent1Id, int parent2Id)
+    {
+      Creature parent1 = _db.Creatures.Where(creature => creature.CreatureId == parent1Id).FirstOrDefault();
+      Creature parent2 = _db.Creatures.Where(creature => creature.CreatureId == parent2Id).FirstOrDefault();
+
+      string EyeCodon = Creature.CodonScrambler(parent1.Eye.Codon, parent2.Eye.Codon);
+      string MouthCodon = Creature.CodonScrambler(parent1.Mouth.Codon, parent2.Mouth.Codon);
+      string HeadCodon = Creature.CodonScrambler(parent1.Head.Codon, parent2.Head.Codon);
+      string TailCodon = Creature.CodonScrambler(parent1.Tail.Codon, parent2.Tail.Codon);
+      // Eye newEye = Creature.ScrambledEyes(parent1.Eye, parent2.Eye);
+      // Mouth newMouth = Creature.MixedUpMouths(parent1.Mouth, parent2.Mouth);
+      // Head newHead = Creature.WedHeads(parent1.Head, parent2.Head);
+      // Tail newTail = Creature.TailTwister(parent1.Tail, parent2.Tail);
+      // Creature newCreature = new Creature();
+
+      // Run scramble methods on parent objects, have new object IDs returned
+
+      
+      
+      // so on and so forth
+
+      // new babyCreature = new Creature (eyeId, )
+
+      // int newEyeId = Creature.ScrambledEyes(parent1.EyeId, parent2.EyeId);
+      // newEye = fetch Eye from database with EyeId
+      // newCreature.Eye = newEye;
+      // newCreature.EyeId = newEyeId;
+
+
+      // int newMouthID = MixedUpMouths(parent1.Mouth, parent2.Mouth);
+      // new Mouth = fetch Mouth from database with Mouth Id
+
+      // Head newHead = WedHeads(parent1.Head, parent2.Head);
+      // Tail newTail = TailTwister(parent1.Tail, parent2.Tail);
+      return View();      
     }
 
     public ActionResult Details(int id)
--- CreatureTeacher/Models/Creature.cs
+++ CreatureTeacher/Models/Creature.cs
@@ -7,22 +7,22 @@ namespace CreatureTeacher.Models
   public class Creature
   {
     public int CreatureId {get;set;}
-    public Creature Parent1 {get;set;}
-    public Creature Parent2 {get;set;}
+    public int Parent1Id {get;set;}
+    public int Parent2Id {get;set;}
     public string Name {get;set;}
 
     public DateTime BirthDate {get;set;}
 
     public int EyeId {get;set;}
-    public virtual Eye Eye {get; set;}
+    public Eye Eye {get; set;}
     public int HeadId {get;set;}
-    public virtual Head Head {get; set;}
+    public Head Head {get; set;}
     public int MouthId {get;set;}
-    public virtual Mouth Mouth {get; set;}
+    public Mouth Mouth {get; set;}
     public int TailId {get;set;}
-    public virtual Tail Tail {get; set;}
+    public Tail Tail {get; set;}
 
-    public Creature(Eye eye, Head head, Mouth mouth, Tail tail, Creature parent1, Creature parent2)
+    public Creature(Eye eye, Head head, Mouth mouth, Tail tail, int parent1Id, int parent2Id)
     {
       this.Name = "New Creature";
       this.BirthDate = DateTime.Now;
@@ -34,64 +34,58 @@ namespace CreatureTeacher.Models
       this.MouthId = mouth.MouthId;
       this.Tail = tail;
       this.TailId = tail.TailId;
-      this.Parent1 = parent1;
-      this.Parent2 = parent2;
-     }
-
-
-    public Creature BreedNew(Creature parent1, Creature parent2)
-    {
-      Eye newEye = ScrambledEyes(parent1.Eye, parent2.Eye);
-      Mouth newMouth = MixedUpMouths(parent1.Mouth, parent2.Mouth);
-      Head newHead = WedHeads(parent1.Head, parent2.Head);
-      Tail newTail = TailTwister(parent1.Tail, parent2.Tail);
-      return new Creature(newEye, newHead, newMouth, newTail, parent1, parent2);
+      this.Parent1Id = parent1Id;
+      this.Parent2Id = parent2Id;
     }
     
-    public Eye ScrambledEyes(Eye eye1, Eye eye2)
+    public static string CodonScrambler(string codon1, string codon2)
     {
-      return Eye;
+      return null;
     }
+    // public static Eye ScrambledEyes(Eye eye1, Eye eye2)
+    // {
+    //   return null;
+    // }
 
-    public Mouth MixedUpMouths(Mouth mouth1, Mouth mouth2)
-    {
-      return Mouth;
-    }
+    // public static Mouth MixedUpMouths(Mouth mouth1, Mouth mouth2)
+    // {
+    //   return null;
+    // }
 
-    public Head WedHeads(Head head1, Head head2)
-    {
-      return Head;
-    }
+    // public static Head WedHeads(Head head1, Head head2)
+    // {
+    //   return null;
+    // }
     
-    public Tail TailTwister(Tail tail1, Tail tail2)
-    {
-      return Tail;
-    }
+    // public static Tail TailTwister(Tail tail1, Tail tail2)
+    // {
+    //   return null;
+    // }
     
   }
 }
 
 
 
-TAT
-TAC 
+// TAT
+// TAC 
 
-if(Parent1Eye.Dominance == true && Parent2Eye.Dominance == true )
-{
-  return Parent1Eye.Codon;
-}
-else if (Parent1Eye.Dominance == true && Parent2Eye.Dominance == false)
-{ 
-  return Parent1Eye.Codon;
-}
-else if (Parent1Eye.Dominance == false && Parent2Eye.Dominance == true)
-{
-  return Parent2Eye.Codon; 
-}
-else if (Parent1Eye.Dominance == false && Parent2Eye.Dominance == false)
-{
-  return Parent2Eye.Codon;
-}
+// if(Parent1Eye.Dominance == true && Parent2Eye.Dominance == true )
+// {
+//   return Parent1Eye.Codon;
+// }
+// else if (Parent1Eye.Dominance == true && Parent2Eye.Dominance == false)
+// { 
+//   return Parent1Eye.Codon;
+// }
+// else if (Parent1Eye.Dominance == false && Parent2Eye.Dominance == true)
+// {
+//   return Parent2Eye.Codon; 
+// }
+// else if (Parent1Eye.Dominance == false && Parent2Eye.Dominance == false)
+// {
+//   return Parent2Eye.Codon;
+// }
 
 
 
